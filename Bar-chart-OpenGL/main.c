0e3ecdd78905d244776d499f5ccfaee0a929ee3c
khaledmohab1 <kmohab71@yahoo.com>
2020-06-08 00:32:57 +0200

    Initial Commit

---
+++ Bar-chart-OpenGL/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Bar-chart-OpenGL
+//
+//  Created by Khaled Mohab on 6/8/20.
+//  Copyright © 2020 Khaled Mohab. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
